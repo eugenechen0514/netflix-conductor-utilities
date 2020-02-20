63a9b57e54cb0e541f7fd6603d62d01cf23d7e06
EugeneChen <yujiechen0514@gmail.com>
2020-02-20 10:41:17 +0800

    fix example

--- example/example.js
+++ example/example.js
@@ -1,4 +1,17 @@
-const {ConductorWorker} = require('../build');
+
+const {ConductorWorker, TaskMetadataManager, WorkflowMetadataManager, WorkflowTaskType, WorkflowManager} = require('../build');
+
+function delay(ms = 10000) {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            resolve();
+        }, ms);
+    });
+}
+
+const taskMetadataManager = new TaskMetadataManager({apiEndpoint: 'http://localhost:8080/api/'});
+const workflowMetadataManager = new WorkflowMetadataManager({apiEndpoint: 'http://localhost:8080/api/'});
+const workflowManager = new WorkflowManager({apiEndpoint: 'http://localhost:8080/api/'});
 
 const worker = new ConductorWorker({
     url: 'http://localhost:8080', // host
@@ -8,19 +21,41 @@ const worker = new ConductorWorker({
 
 const taskType = 'test';
 
-const fn = (input) => {
-    return new Promise((resolve, reject) => {
-      const handler = setTimeout(()=>{
-        clearTimeout(handler);
-        resolve({
-            result: false,
+(async () => {
+    await taskMetadataManager.registerTask({
+        name: taskType,
+    });
+
+    let wfName = `test_wf_${taskType}`;
+    await workflowMetadataManager.registerWorkflow({
+        name: wfName,
+        tasks: [
+            {
+                "name": taskType,
+                "taskReferenceName": taskType,
+                "type": WorkflowTaskType.simple,
+            }
+        ],
+    });
+
+    const fn = (input) => {
+        return new Promise((resolve, reject) => {
+            const handler = setTimeout(()=>{
+                clearTimeout(handler);
+                resolve({
+                    result: false,
+                })
+            }, 3000)
         })
-      }, 3000)
-    })
-  };
+    };
 
-worker.start(taskType, fn);
+    worker.start(taskType, fn);
 
-setTimeout(()=>{
-  worker.stop()
-}, 10000);
+    await workflowManager.startWorkflow({
+        name: wfName,
+    });
+
+    await delay(10000);
+    worker.stop();
+})()
+    .then(console.log);
