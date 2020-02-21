3a9ee96f043ef69d10ba3527a47923ec9d36ca8a
EugeneChen <yujiechen0514@gmail.com>
2020-02-21 17:15:23 +0800

    upgrade version: 0.5.0

--- dist/ConductorWorker.js
+++ dist/ConductorWorker.js
@@ -18,16 +18,21 @@ const run_forever_1 = require("run-forever");
 const delay_1 = __importDefault(require("delay"));
 const axios_1 = __importDefault(require("axios"));
 const _1 = require("./");
-const debug = debug_1.default('ConductorWorker');
-const debugError = debug_1.default('ConductorWorker');
+const debug = debug_1.default('ConductorWorker[DEBUG]');
+const debugError = debug_1.default('ConductorWorker[Error]');
 class ConductorWorker extends events_1.EventEmitter {
     constructor(options = {}) {
         super();
-        this.working = false;
-        const { url = 'http://localhost:8080', apiPath = '/api', workerid = undefined } = options;
+        this.polling = false;
+        this.maxConcurrent = Number.POSITIVE_INFINITY;
+        this.runningTasks = [];
+        const { url = 'http://localhost:8080', apiPath = '/api', workerid = undefined, maxConcurrent } = options;
         this.url = url;
         this.apiPath = apiPath;
         this.workerid = workerid;
+        if (maxConcurrent) {
+            this.maxConcurrent = maxConcurrent;
+        }
         this.client = axios_1.default.create({
             baseURL: this.url,
             responseType: 'json',
@@ -35,7 +40,10 @@ class ConductorWorker extends events_1.EventEmitter {
     }
     pollAndWork(taskType, fn) {
         return (() => __awaiter(this, void 0, void 0, function* () {
+            // NOTE: There is a potential problem which is 「poll task」 and 「ack task」 should be as soon as possible,
+            //  if no two workers maybe deal with simultaneously when they poll the same task at the same time.
             // Poll for Worker task
+            debug(`Poll a "${taskType}" task`);
             const { data: pullTask } = yield this.client.get(`${this.apiPath}/tasks/poll/${taskType}?workerid=${this.workerid}`);
             if (!pullTask) {
                 return;
@@ -43,25 +51,35 @@ class ConductorWorker extends events_1.EventEmitter {
             const input = pullTask.inputData;
             const { workflowInstanceId, taskId } = pullTask;
             // Ack the Task
+            debug(`Ack the "${taskType}" task`);
             yield this.client.post(`${this.apiPath}/tasks/${taskId}/ack?workerid=${this.workerid}`);
+            // Record running task
+            this.runningTasks.push(taskId);
             const t1 = Date.now();
             const baseTaskInfo = {
                 workflowInstanceId,
                 taskId,
             };
             // Working
+            debug('Dealing with the task:', { workflowInstanceId, taskId });
             return fn(input)
                 .then(output => {
+                debug('worker resolve');
                 return Object.assign(Object.assign({}, baseTaskInfo), { callbackAfterSeconds: (Date.now() - t1) / 1000, outputData: output, status: _1.TaskState.completed });
             })
                 .catch((err) => {
+                debug('worker reject', err);
                 return Object.assign(Object.assign({}, baseTaskInfo), { callbackAfterSeconds: (Date.now() - t1) / 1000, reasonForIncompletion: String(err), status: _1.TaskState.failed });
             })
                 .then(updateTaskInfo => {
+                // release running task
+                this.runningTasks = this.runningTasks.filter(taskId => taskId !== updateTaskInfo.taskId);
+                debug(`Change the amount of running tasks: ${this.runningTasks.length}`);
                 // Return response, add logs
+                debug('update task info: taskId:' + taskId);
                 return this.client.post(`${this.apiPath}/tasks/`, updateTaskInfo)
                     .then(result => {
-                    debug(result);
+                    // debug(result.data);
                 })
                     .catch(err => {
                     debugError(err); // resolve
@@ -70,27 +88,32 @@ class ConductorWorker extends events_1.EventEmitter {
         }))();
     }
     start(taskType, fn, interval = 1000) {
-        this.working = true;
-        debug(`Start worker: taskType = ${taskType}, poll-interval = ${interval}`);
+        this.polling = true;
+        debug(`Start polling taskType = ${taskType}, poll-interval = ${interval}, maxConcurrent = ${this.maxConcurrent}`);
         run_forever_1.forever(() => __awaiter(this, void 0, void 0, function* () {
-            if (this.working) {
+            if (this.polling) {
                 yield delay_1.default(interval);
-                debug(`Poll "${taskType}" task`);
-                this.pollAndWork(taskType, fn)
-                    .then((data) => {
-                    // debug(data);
-                }, (err) => {
-                    debugError(err);
-                });
+                debug(`Check the amount of running tasks: ${this.runningTasks.length}`);
+                if (this.runningTasks.length < this.maxConcurrent) {
+                    this.pollAndWork(taskType, fn)
+                        .then((data) => {
+                        // debug(data);
+                    }, (err) => {
+                        debugError(err);
+                    });
+                }
+                else {
+                    debug(`Skip polling task because the work reaches the max amount of running tasks`);
+                }
             }
             else {
-                debug(`End worker: taskType = ${taskType}`);
+                debug(`Stop polling: taskType = ${taskType}`);
                 return run_forever_1.END;
             }
         }));
     }
     stop() {
-        this.working = false;
+        this.polling = false;
     }
 }
 exports.ConductorWorker = ConductorWorker;
--- dist/WorkflowManager.js
+++ dist/WorkflowManager.js
@@ -8,6 +8,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -39,6 +50,92 @@ class WorkflowManager {
             const { data: workflowId } = yield this.client.post('/workflow', options);
             const workflow = yield this.retrieveWorkflow(workflowId);
             assert_1.default(workflow.workflowId === workflowId, 'Start a workflow, but can not find it');
+            return workflow;
+        });
+    }
+    terminateWorkflow(workflowId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const workflow = yield this.retrieveWorkflow(workflowId);
+            yield this.client.delete('/workflow/' + workflowId);
+            return workflow;
+        });
+    }
+    removeWorkflow(workflowId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const workflow = yield this.retrieveWorkflow(workflowId);
+            yield this.client.delete('/workflow/' + workflowId + '/remove');
+            return workflow;
+        });
+    }
+    /**
+     * Pause. No further tasks will be scheduled until resumed. Currently running tasks are not paused.
+     * @param workflowId
+     */
+    pauseWorkflow(workflowId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const workflow = yield this.retrieveWorkflow(workflowId);
+            yield this.client.put('/workflow/' + workflowId + '/pause');
+            return workflow;
+        });
+    }
+    /**
+     * Resume normal operations after a pause.
+     * @param workflowId
+     */
+    resumeWorkflow(workflowId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const workflow = yield this.retrieveWorkflow(workflowId);
+            yield this.client.put('/workflow/' + workflowId + '/resume');
+            return workflow;
+        });
+    }
+    /**
+     * Re-runs a completed workflow from a specific task.
+     * @param workflowId
+     * @param options
+     */
+    rerunWorkflow(workflowId, options) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const { data } = yield this.client.post('/workflow/' + workflowId + '/rerun', options);
+            const workflow = yield this.retrieveWorkflow(workflowId);
+            return workflow;
+        });
+    }
+    /**
+     * Restart workflow execution from the start. Current execution history is wiped out.
+     * @param workflowId
+     */
+    restartWorkflow(workflowId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const workflow = yield this.retrieveWorkflow(workflowId);
+            yield this.client.post('/workflow/' + workflowId + '/restart');
+            return workflow;
+        });
+    }
+    /**
+     * Retry the last failed task.
+     * @param workflowId
+     */
+    retryWorkflow(workflowId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const workflow = yield this.retrieveWorkflow(workflowId);
+            yield this.client.post('/workflow/' + workflowId + '/retry');
+            return workflow;
+        });
+    }
+    /**
+     * Skips a task execution (specified as taskReferenceName parameter) in a running workflow and continues forward.
+     * Optionally updating task's input and output as specified in the payload.
+     * PUT /workflow/{workflowId}/skiptask/{taskReferenceName}?workflowId=&taskReferenceName=
+     * @param workflowId
+     * @param options
+     */
+    skipWorkflowTask(workflowId, options) {
+        return __awaiter(this, void 0, void 0, function* () {
+            assert_1.default(options.taskReferenceName, 'taskReferenceName should be not empty');
+            const { taskReferenceName } = options, others = __rest(options, ["taskReferenceName"]);
+            const { data } = yield this.client.put('/workflow/' + workflowId + '/skiptask/' + taskReferenceName, others);
+            const workflow = yield this.retrieveWorkflow(workflowId);
             return workflow;
         });
     }
