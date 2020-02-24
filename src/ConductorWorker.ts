21355dc29c3b2928eb2485fb2b964000c1375495
EugeneChen <yujiechen0514@gmail.com>
2020-02-24 15:01:32 +0800

    refactor

--- src/ConductorWorker.ts
+++ src/ConductorWorker.ts
@@ -57,14 +57,25 @@ class ConductorWorker<Result = void> extends EventEmitter {
     });
   }
 
+  __canPollTask() {
+    debug(`Check the amount of running tasks: ${this.runningTasks.length}`);
+    if(this.runningTasks.length >= this.maxConcurrent) {
+      debug(`Skip polling task because the work reaches the max amount of running tasks`);
+      return false;
+    }
+    return true
+  }
+
   pollAndWork(taskType: string, fn: WorkFunction<Result>) { // keep 'function'
     return (async () => {
       // Poll for Worker task
+      debug(`Poll a "${taskType}" task`);
       const {data: pullTask} = await this.client.get<PollTask | void>(`${this.apiPath}/tasks/poll/${taskType}?workerid=${this.workerid}`);
       if (!pullTask) {
+        debug(`No more "${taskType}" tasks`);
         return;
       }
-      debug(`Poll a "${taskType}" task`, pullTask);
+      debug(`Polled a "${taskType}" task: `, pullTask);
       const input = pullTask.inputData;
       const { workflowInstanceId, taskId } = pullTask;
 
@@ -138,18 +149,15 @@ class ConductorWorker<Result = void> extends EventEmitter {
     debug(`Start polling taskType = ${taskType}, poll-interval = ${interval}, maxConcurrent = ${this.maxConcurrent}`);
     forever(async () => {
       if (this.polling) {
-        await delay(interval);
-        debug(`Check the amount of running tasks: ${this.runningTasks.length}`);
-        if(this.runningTasks.length < this.maxConcurrent) {
+        if(this.__canPollTask()) {
           this.pollAndWork(taskType, fn)
               .then((data: any) => {
                 // debug(data);
               }, (err: any) => {
                 debugError(err)
               });
-        } else {
-          debug(`Skip polling task because the work reaches the max amount of running tasks`);
         }
+        await delay(interval);
       } else {
         debug(`Stop polling: taskType = ${taskType}`);
         return END;
