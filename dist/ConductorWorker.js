ce0f29cc374560efa8e4cd712c576d65bc5fb053
EugeneChen <yujiechen0514@gmail.com>
2020-02-24 15:33:59 +0800

    upgrade version: 0.5.1

--- dist/ConductorWorker.js
+++ dist/ConductorWorker.js
@@ -26,54 +26,76 @@ class ConductorWorker extends events_1.EventEmitter {
         this.polling = false;
         this.maxConcurrent = Number.POSITIVE_INFINITY;
         this.runningTasks = [];
-        const { url = 'http://localhost:8080', apiPath = '/api', workerid = undefined, maxConcurrent } = options;
+        this.heartbeatInterval = 300000; //default: 5 min
+        const { url = 'http://localhost:8080', apiPath = '/api', workerid = undefined, maxConcurrent, heartbeatInterval } = options;
         this.url = url;
         this.apiPath = apiPath;
         this.workerid = workerid;
         if (maxConcurrent) {
             this.maxConcurrent = maxConcurrent;
         }
+        if (heartbeatInterval) {
+            this.heartbeatInterval = heartbeatInterval;
+        }
         this.client = axios_1.default.create({
             baseURL: this.url,
             responseType: 'json',
         });
     }
+    __canPollTask() {
+        debug(`Check the amount of running tasks: ${this.runningTasks.length}`);
+        if (this.runningTasks.length >= this.maxConcurrent) {
+            debug(`Skip polling task because the work reaches the max amount of running tasks`);
+            return false;
+        }
+        return true;
+    }
     pollAndWork(taskType, fn) {
         return (() => __awaiter(this, void 0, void 0, function* () {
-            // NOTE: There is a potential problem which is 「poll task」 and 「ack task」 should be as soon as possible,
-            //  if no two workers maybe deal with simultaneously when they poll the same task at the same time.
             // Poll for Worker task
             debug(`Poll a "${taskType}" task`);
             const { data: pullTask } = yield this.client.get(`${this.apiPath}/tasks/poll/${taskType}?workerid=${this.workerid}`);
             if (!pullTask) {
+                debug(`No more "${taskType}" tasks`);
                 return;
             }
+            debug(`Polled a "${taskType}" task: `, pullTask);
             const input = pullTask.inputData;
             const { workflowInstanceId, taskId } = pullTask;
             // Ack the Task
             debug(`Ack the "${taskType}" task`);
             yield this.client.post(`${this.apiPath}/tasks/${taskId}/ack?workerid=${this.workerid}`);
             // Record running task
-            this.runningTasks.push(taskId);
-            const t1 = Date.now();
+            const runningTask = {
+                taskId,
+                done: false,
+                start: Date.now(),
+            };
+            this.runningTasks.push(runningTask);
+            debug(`Create runningTask: `, runningTask);
             const baseTaskInfo = {
                 workflowInstanceId,
                 taskId,
             };
             // Working
             debug('Dealing with the task:', { workflowInstanceId, taskId });
+            // const runningTask = this.__forceFindOneProcessingTask(taskId);
             return fn(input)
                 .then(output => {
                 debug('worker resolve');
-                return Object.assign(Object.assign({}, baseTaskInfo), { callbackAfterSeconds: (Date.now() - t1) / 1000, outputData: output, status: _1.TaskState.completed });
+                runningTask.done = true;
+                debug(`Update runningTask:`, runningTask);
+                return Object.assign(Object.assign({}, baseTaskInfo), { callbackAfterSeconds: 0, outputData: output, status: _1.TaskState.completed });
             })
                 .catch((err) => {
                 debug('worker reject', err);
-                return Object.assign(Object.assign({}, baseTaskInfo), { callbackAfterSeconds: (Date.now() - t1) / 1000, reasonForIncompletion: String(err), status: _1.TaskState.failed });
+                runningTask.done = true;
+                debug(`Update runningTask:`, runningTask);
+                return Object.assign(Object.assign({}, baseTaskInfo), { callbackAfterSeconds: 0, reasonForIncompletion: String(err), status: _1.TaskState.failed });
             })
                 .then(updateTaskInfo => {
                 // release running task
-                this.runningTasks = this.runningTasks.filter(taskId => taskId !== updateTaskInfo.taskId);
+                this.runningTasks = this.runningTasks.filter(task => task.taskId !== updateTaskInfo.taskId);
                 debug(`Change the amount of running tasks: ${this.runningTasks.length}`);
                 // Return response, add logs
                 debug('update task info: taskId:' + taskId);
@@ -92,9 +114,7 @@ class ConductorWorker extends events_1.EventEmitter {
         debug(`Start polling taskType = ${taskType}, poll-interval = ${interval}, maxConcurrent = ${this.maxConcurrent}`);
         run_forever_1.forever(() => __awaiter(this, void 0, void 0, function* () {
             if (this.polling) {
-                yield delay_1.default(interval);
-                debug(`Check the amount of running tasks: ${this.runningTasks.length}`);
-                if (this.runningTasks.length < this.maxConcurrent) {
+                if (this.__canPollTask()) {
                     this.pollAndWork(taskType, fn)
                         .then((data) => {
                         // debug(data);
@@ -102,9 +122,7 @@ class ConductorWorker extends events_1.EventEmitter {
                         debugError(err);
                     });
                 }
-                else {
-                    debug(`Skip polling task because the work reaches the max amount of running tasks`);
-                }
+                yield delay_1.default(interval);
             }
             else {
                 debug(`Stop polling: taskType = ${taskType}`);
