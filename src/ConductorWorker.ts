53d9c69eebbe639e866590272a2127b32a153be9
EugeneChen <yujiechen0514@gmail.com>
2021-12-10 14:34:00 +0800

    [ADD] add KeepTaskTimer

--- src/ConductorWorker.ts
+++ src/ConductorWorker.ts
@@ -1,7 +1,6 @@
-import assert from 'assert';
-import debugFun  from 'debug';
+import debugFun from 'debug';
 import {EventEmitter} from 'events';
-import {forever, END} from 'run-forever';
+import {END, forever} from 'run-forever';
 import delay from 'delay';
 
 import axios, {AxiosInstance} from 'axios';
@@ -25,19 +24,36 @@ export interface ConductorWorkerOptions {
 
 export type WorkFunction<Result = void> = (input: any, runningTask: RunningTask<Result>) => Promise<Result>;
 
+interface KeepTaskTimerOptions {
+    // ms
+    // default: 10000
+    interval: number;
+
+    // second
+    // need to " < responseTimeoutSeconds"
+    // Ref: https://netflix.github.io/conductor/configuration/taskdef/#task-definition
+    // default: 60
+    callbackAfterSeconds: number;
+}
+
+export type RunningTaskOptions = RunningTaskCoreInfo & KeepTaskTimerOptions;
+
 class RunningTask<Result = void> {
-    options: RunningTaskCoreInfo;
+    options: RunningTaskOptions;
     worker: ConductorWorker<Result>;
     done: boolean;
     start: number | undefined;
+    keepRunningTimer: NodeJS.Timeout | undefined;
 
-    constructor(worker: ConductorWorker<Result>, options: RunningTaskCoreInfo) {
+    constructor(worker: ConductorWorker<Result>, options: RunningTaskCoreInfo & Partial<KeepTaskTimerOptions>) {
         this.worker = worker;
-        this.options = options;
+
+        const {interval = 10000, callbackAfterSeconds = 60} = options;
+        this.options = {...options, interval, callbackAfterSeconds};
         this.done = false;
     }
 
-    updateTaskInfo(partialUpdateTaskInfo: Partial<UpdatingTaskResult> & {callbackAfterSeconds: number, status: TaskState}) {
+    updateTaskInfo(partialUpdateTaskInfo: Partial<UpdatingTaskResult> & {status: TaskState}) {
         const updateTaskInfo = {...this.options, ...partialUpdateTaskInfo};
 
         const {client, apiPath} = this.worker;
@@ -47,9 +63,37 @@ class RunningTask<Result = void> {
     startTask() {
         this.start = Date.now();
         this.done = false;
+
+        this.__setKeepTaskTimerForNotifyConductor();
+    }
+
+    private __setKeepTaskTimerForNotifyConductor() {
+        // clean old timer
+        this.__clearKeepTaskTimerForNotifyConductor();
+
+        // new a timer
+        // notify conductor: task is still running, and not put the queue back
+        this.keepRunningTimer = setInterval(() => {
+            const callbackAfterSeconds = this.options.callbackAfterSeconds;
+            this.updateTaskInfo({
+                status: TaskState.inProgress,
+                callbackAfterSeconds,
+            })
+                .catch(error => {
+                    debugError(error);
+                })
+        }, this.options.interval);
+    }
+
+    private __clearKeepTaskTimerForNotifyConductor() {
+        if (this.keepRunningTimer) {
+            clearInterval(this.keepRunningTimer);
+            this.keepRunningTimer = undefined;
+        }
     }
 
     stopTask() {
+        this.__clearKeepTaskTimerForNotifyConductor();
         this.done = true;
     }
 }
