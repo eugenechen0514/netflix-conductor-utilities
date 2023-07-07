0b324efaf96681b196cb137e8049d254b3774507
EugeneChen <yujiechen0514@gmail.com>
2023-07-07 13:40:49 +0800

    fix: ignore dist folder

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './types';
-export * from './TaskMetadataManager';
-export * from './WorkflowMetadataManager';
-export * from './WorkflowManager';
-export * from './ConductorWorker';
--- dist/utils/logger.d.ts
+++
@@ -1,4 +0,0 @@
-import initDebug from 'debug';
-export declare const base: initDebug.Debugger;
-export declare const debug: initDebug.Debugger;
-export declare const error: initDebug.Debugger;
